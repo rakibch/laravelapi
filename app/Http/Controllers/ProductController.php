d95bc3e288c6c283063315dbb34e522bbfa95721
Rakib <rakib9204@gmail.com>
2020-02-14 20:45:43 +0600

    Product Collection Resource

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Model\Product;
+use App\Http\Resources\Product\ProductResource;
+use App\Http\Resources\Product\ProductCollection;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
@@ -14,7 +16,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-        //
+        return ProductCollection::collection(Product::all());
     }
 
     /**
@@ -46,7 +48,7 @@ class ProductController extends Controller
      */
     public function show(Product $product)
     {
-        //
+        return new ProductResource($product);
     }
 
     /**
---
+++ app/Http/Resources/Product/ProductResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources\Product;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProductResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+          'name'=>$this->name,
+          'description1'=>$this->detail,
+          'price'=>$this->price,
+          'stock'=>$this->stock==0 ? 'Out of stock' : $this->stock,
+          'totalPrice'=>round((1-($this->discount/100)) * $this->price,2),
+          'rating'=>$this->reviews->count()> 0 ? round($this->reviews->sum('star')/$this->reviews->count(),2) : 'No rating Yet',
+          'href'=>[
+              'reviews'=>route('reviews.index',$this->id)
+          ]
+        ];
+    }
+}
